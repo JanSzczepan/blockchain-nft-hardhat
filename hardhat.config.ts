d3679366fe39d0ec7f6f97a7a0a4d1b752572a0c
Jan Szczepan <janszczepan62@gmail.com>
2023-05-16 12:18:22 +0200

    Hardhat config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-deploy'
 
 const config: HardhatUserConfig = {
+   defaultNetwork: 'hardhat',
+   networks: {
+      hardhat: {
+         chainId: 31337,
+      },
+   },
    solidity: '0.8.18',
+   namedAccounts: {
+      deployer: {
+         default: 0,
+         1: 0,
+      },
+   },
 }
 
 export default config
